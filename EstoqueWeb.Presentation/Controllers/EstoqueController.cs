2b99c8b4edb3b2360cdee4fe98f4e2af3d2c626d
Antonio Barbosa <acrb1108@gmail.com>
2023-08-31 21:29:44 -0300

    Iniciando o projeto Estoque Web

---
+++ EstoqueWeb.Presentation/Controllers/EstoqueController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EstoqueWeb.Presentation.Controllers
+{
+    public class EstoqueController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Consulta()
+        {
+            return View();
+        }
+
+        public IActionResult Relatorio()
+        {
+            return View();
+        }
+    }
+}
